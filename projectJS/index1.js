51875ea05a7de592f89919b5b2b69b984a30f140
Emre SAGLAM <69041945+emresaglamtv@users.noreply.github.com>
2023-08-28 19:53:44 +0300

    Add files via upload

---
+++ projectJS/index1.js
@@ -0,0 +1,44 @@
+//Update
+const div1 = document.getElementById("pUpdate");
+const button1 = document.getElementById("updateButton");
+button1.addEventListener("click", function(){
+pUpdate.classList.toggle("colorp");
+})
+
+div1.classList.add("update");
+const classNames1 = div1.classList;
+document.getElementById("pUpdate").innerHTML="class eklendikten sonra ki liste:" + classNames1;
+
+//Add
+const div=document.getElementById("add");
+div.classList.add("fancy","add","addMetod");
+
+const classNames = div.classList;
+document.getElementById("pAdd").innerHTML="Class ekledikten sonra liste: "+ classNames;
+
+//Remove
+div.classList.remove("addMetod");
+document.getElementById("pRemove").innerHTML="Class sildikten sonra liste: "+ classNames;
+
+//Item
+ const firstClassName=div.classList.item(0);
+ document.getElementById("pItem").innerHTML= "İlk class: "+ firstClassName;
+
+//Contains
+const result=div.classList.contains("add");
+document.getElementById("pContainsTrue").innerHTML="'add' class'ını içeriyor mu? "+" --> "+ result;
+const result2=div.classList.contains("addMetod");
+document.getElementById("pContainsFalse").innerHTML="'addMetod' class'ını içeriyor mu? "+" --> "+ result2;
+
+//Replace
+div.classList.replace("add","newClass");
+document.getElementById("pReplace").innerHTML=classNames;
+
+//Toggle
+const pToggle=document.getElementById("pToggle")
+const button=document.getElementById("toggleButton");
+button.addEventListener("click",function(){
+pToggle.classList.toggle("colorp");
+})
+//Length
+document.getElementById("length").innerHTML="Class names length:"+" "+div.classList.length;
